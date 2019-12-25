4a59bee7e69621c6689b5fe9c056eb5415c2ebb6
KoyFoster <koyyashina@hotmail.com>
2019-12-24 20:06:03 -0600

    Stats and Labels

--- src/Components/Diagram.js
+++ src/Components/Diagram.js
@@ -10,13 +10,14 @@ var chart =
     iScale: 150
 };
 
+//User inputted stats as semi-colin dilimited strings
+var statTypes = ["POWER","SPEED","RANGE","DURABILITY","PRECISION","POTENTIAL","???"];
+var stats = [8,(Math.random() * (8 - 1) + 1),(Math.random() * (8 - 1) + 1),(Math.random() * (8 - 1) + 1),(Math.random() * (8 - 1) + 1),(Math.random() * (8 - 1) + 1),(Math.random() * (8 - 1) + 1)];
+
 var iDim = [560, 350];
 var iCore = [iDim[0]/2, iDim[1]/2];
 var meshTrans;
 
-//User inputted stats as semi-colin dilimited strings
-var statTypes = "1;2;3;4;5;6";
-var stats = "1;3;1;5;1;7";
 
 /*Update Diagram*/
 var setupDiagram = function(props)
@@ -49,7 +50,7 @@ var setupDiagram = function(props)
     }    
     //Offset Coordinates
     meshTrans = mesh.map(function (arr) { return [iCore[0] + arr[0], iCore[1] + arr[1]]; });
-    return <polygon points={meshTrans} style={{fill: "white", fillOpacity: 0.5, stroke: "black", strokeWidth: 0.25, fillRule: "evenodd"}} />;
+    return <polygon points={meshTrans} style={{fill: "white", fillOpacity: 0.5, stroke: "black", strokeWidth: 1, fillRule: "evenodd"}} />;
 };
 
 var setupStats = function(props)
@@ -66,13 +67,14 @@ var setupStats = function(props)
     var lastCorner = new Vector2();
     var firstCorner = new Vector2();
     var mesh = [];
-    for (var i = 1; i < chart.iParts+1; i++)
+    for (var i = 0; i < chart.iParts; i++)
     {
-        var rand = (Math.random() * (10 - 1) + 1)/10;
-        var v2Polygon = [new Vector2(0,0), new Vector2(0, (1*chart.iScale)/2), new Vector2(0, (rand*chart.iScale)/2)];
+        var v2Polygon;
         //Adjust Angles
-        if(i === 1)
+        if(i === 0)
         {
+            v2Polygon = [new Vector2(0,0), new Vector2(0, (-stats[0]*0.1*chart.iScale)), new Vector2(0, (-stats[1]*0.1*chart.iScale))];
+
             Coll.v2Rotate2D(v2Polygon[1],v2Polygon[0],iDegrees*i);
             Coll.v2Rotate2D(v2Polygon[2],v2Polygon[0],iDegrees*(i+1));
 
@@ -80,13 +82,24 @@ var setupStats = function(props)
 
             firstCorner = v2Polygon[1];
         }
-        else //if(i < 4)
+        else if(i !== chart.iParts-1)
         {
+            v2Polygon = [new Vector2(0,0), new Vector2(0, (-stats[i-1]*0.1*chart.iScale)), new Vector2(0, (-stats[i]*0.1*chart.iScale))];
+
             v2Polygon[1] = lastCorner;
             Coll.v2Rotate2D(v2Polygon[2],v2Polygon[0],iDegrees*(i+1));
 
             mesh.push([v2Polygon[0].x, v2Polygon[0].y]); mesh.push([v2Polygon[1].x, v2Polygon[1].y]); mesh.push([v2Polygon[2].x, v2Polygon[2].y]);
         }
+        else
+        {
+            v2Polygon = [new Vector2(0,0), new Vector2(0, (-stats[i]*0.1*chart.iScale)), new Vector2(0, (-stats[0]*0.1*chart.iScale))];
+            v2Polygon[1] = lastCorner;
+            Coll.v2Rotate2D(v2Polygon[2],v2Polygon[0],iDegrees*(i+1));
+
+            mesh.push([v2Polygon[0].x, v2Polygon[0].y]); mesh.push([v2Polygon[1].x, v2Polygon[1].y]); mesh.push([v2Polygon[2].x, v2Polygon[2].y]);
+        }
+        
         lastCorner = v2Polygon[2];
         if(i === chart.iParts)
         {
@@ -98,10 +111,27 @@ var setupStats = function(props)
 
 
 
-    var htmlStats = <polygon points={meshTrans} fill = "url(#grad)" style={{fillOpacity: 0.66, stroke: "red", strokeWidth: 0.25, fillRule: "evenodd"}} />;
+    var htmlStats = <polygon points={meshTrans} fill = "url(#grad)" style={{fillOpacity: 0.66, stroke: "red", strokeWidth: 0, fillRule: "evenodd"}} />;
 
     return htmlStats;
 };
+
+
+var SetupText = function(props)
+{
+    //Window Dimensions Window Center
+    var iDim = [560, 350];
+    var iCore = [iDim[0]/2, iDim[1]/2];
+    var iDegrees = (360/chart.iParts);
+
+    var htmlResult = [];
+    for (var i = 0; i < chart.iParts; i++)
+    {        
+        htmlResult.push( <text text-anchor="middle" x={iCore[0]} y={iCore[1]-chart.iScale-10} transform={"rotate("+i*iDegrees+", "+iCore[0]+","+iCore[1]+")"} > {statTypes[i]} </text>); 
+    }
+return <a>{htmlResult}</a>
+};
+
 
 class Diagram extends React.Component
 {
@@ -114,15 +144,16 @@ class Diagram extends React.Component
             <div>
                 <div>
                     <svg width={iDim[0]} height={iDim[1]} >
-                        <circle cx={iCore[0]} cy={iCore[1]} r={1*chart.iScale} style={{fill: "white", fillOpacity: 0.5, stroke: "black", strokeWidth: 0.5}} />
+                        <circle cx={iCore[0]} cy={iCore[1]} r={1*chart.iScale} style={{fill: "white", fillOpacity: 0.5, stroke: "black", strokeWidth: 2}} />
                         <defs>
                             <linearGradient id = "grad">
-                                <stop offset="0" stop-color="red"/>
-                                <stop offset="1" stop-color="blue"/>
+                                <stop offset="0" stop-color="purple"/>
+                                <stop offset="1" stop-color="lightBlue"/>
                             </linearGradient>
                         </defs>
                         {setupDiagram()}
                         {setupStats()}
+                        {SetupText()}
                     </svg>
                 </div>
                 <Box display="flexbox" border="2px solid #3f0000" bgcolor="darkGrey" color="#a4a4a4" width={iDim[0]} height={iDim[1]}>
