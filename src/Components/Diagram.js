83034a21c6bd5a58a12035fda1b25085bff4e875
KoyFoster <koyyashina@hotmail.com>
2019-12-27 23:17:05 -0600

    State Handling FIxes In-Progress

--- src/Components/Diagram.js
+++ src/Components/Diagram.js
@@ -3,101 +3,63 @@ import { Box } from '@material-ui/core';
 import {Vector2,  Coll} from './KoyMath.js';
 import StatInputCtrls from "./StatInputCtrls.js"
 
-var chart = 
-{ 
-    iParts: 6,
-    iScale: 300
-};
-
-//Window Dimensions Window Center
-var iDegrees = (360/chart.iParts);
-var iDim = [chart.iScale*3, chart.iScale*3];
-var iCore = [iDim[0]/2, iDim[1]/2];
-var meshTrans;
-
-
-//User inputted stats as semi-colin dilimited strings
-var statData = 
-{
-    MaxPoints:  100,
-    Ranges:     [[0,8], [0,10], [0,7],      [0,10],        [0,9],       [0,2],      [0,10], [0,10], [0,10], [0,10]],
-    Types:      ["POWER","SPEED","RANGE",   "DURABILITY",  "PRECISION", "POTENTIAL","???"],
-    Values:     [0,0,0,0,0,0,0,0,0,0,0,0,0],
-    letterGrades:   ['F','E','D','C','B','A','S','?'],
-    statGrades:     [0,0,0,0,0,0,0,0,0,0,0,0,0],
-}
-statData.Values =
-    [(Math.random() * (statData.Ranges[0][1] - 1) + 1),
-    (Math.random() * (statData.Ranges[0][1] - 1) + 1),
-    (Math.random() * (statData.Ranges[2][1] - 1) + 1),
-    (Math.random() * (statData.Ranges[3][1] - 1) + 1),
-    (Math.random() * (statData.Ranges[4][1] - 1) + 1),
-    (Math.random() * (statData.Ranges[5][1] - 1) + 1),
-    (Math.random() * (statData.Ranges[6][1] - 1) + 1)];
+const iDiagramScale = 256;
 
 //Grade Function
 var gradeCalc = function(index)
-{
-    var result = statData.letterGrades[7];//? grade
+{   
+    var result = this.state.letterGrades[7];//? grade
 
     //Take Values and divide it against it's max range
-    var percentage = (statData.Values[index]/statData.Ranges[index][1])*100;
+    var percentage = (this.state.Values[index]/this.state.Ranges[index][1])*100;
 
     //Upper or lower value assignment
     var masymenos = (value) => 
     {
-        if(value > 10)
-        {
-            result += '+';
-        }
-        else if(value < 10)
-        {
-            result += '-';            
-        }
+        if(value > 10){result += '+';} else if(value < 10){result += '-';}
     };
 
     //Letter Assignment
     if(percentage > 100)
     {
-        result = statData.letterGrades[7];
+        result = this.state.letterGrades[7];
         masymenos(percentage-80);
     }
     if(percentage === 100)
     {
-        result = statData.letterGrades[6];
+        result = this.state.letterGrades[6];
         masymenos(percentage-80);
     }
     else if(percentage >= 80)
     {
-        result = statData.letterGrades[5];
+        result = this.state.letterGrades[5];
         masymenos(percentage-80);
     }
     else if(percentage >= 60)
     {
-        result = statData.letterGrades[4];
+        result = this.state.letterGrades[4];
         masymenos(percentage-60);
     }
     else if(percentage >= 40)
     {
-        result = statData.letterGrades[3];
+        result = this.state.letterGrades[3];
         masymenos(percentage-40);
     }
     else if(percentage >= 20)
     {        
-        result = statData.letterGrades[2];
+        result = this.state.letterGrades[2];
         masymenos(percentage-20);
     }
     else if(percentage >= 0)
     {
-        result = statData.letterGrades[1];
+        result = this.state.letterGrades[1];
         masymenos(percentage);
     }
     else if(percentage === 0)
     {
-        result = statData.letterGrades[0];
+        result = this.state.letterGrades[0];
         masymenos(percentage);
     }
-
     return result;//Return '?'
 }
 
@@ -105,26 +67,25 @@ var gradeCalc = function(index)
 var setupDiagram = function(props)
 {
     /*Iterate For Each Triangle*/
-    var v2Polygon = [new Vector2(0,0), new Vector2(0, -1*chart.iScale), new Vector2(0, -1*chart.iScale)];
+    var v2Polygon = [new Vector2(0,0), new Vector2(0, -1*iDiagramScale), new Vector2(0, -1*iDiagramScale)];
     var mesh = [];
-    for (var i = 1; i < chart.iParts+1; i++)
+    for (var i = 1; i < this.state.Quantity+1; i++)
     {
         //Adjust Angles
         if(i === 1)
         {
-            Coll.v2Rotate2D(v2Polygon[2],v2Polygon[0],iDegrees)
+            Coll.v2Rotate2D(v2Polygon[2],v2Polygon[0],this.state.iDegrees)
             mesh.push([v2Polygon[0].x, v2Polygon[0].y]); mesh.push([v2Polygon[1].x, v2Polygon[1].y]); mesh.push([v2Polygon[2].x, v2Polygon[2].y]);
         }
         else
         {
-            Coll.v2Rotate2D(v2Polygon[1],v2Polygon[0],iDegrees);
-            Coll.v2Rotate2D(v2Polygon[2],v2Polygon[0],iDegrees);
+            Coll.v2Rotate2D(v2Polygon[1],v2Polygon[0],this.state.iDegrees);
+            Coll.v2Rotate2D(v2Polygon[2],v2Polygon[0],this.state.iDegrees);
             mesh.push([v2Polygon[0].x, v2Polygon[0].y]); mesh.push([v2Polygon[1].x, v2Polygon[1].y]); mesh.push([v2Polygon[2].x, v2Polygon[2].y]);
         }
     }    
     //Offset Coordinates
-    meshTrans = mesh.map(function (arr) { return [iCore[0] + arr[0], iCore[1] + arr[1]]; });
-    return <polygon points={meshTrans} style={{fill: "white", fillOpacity: 0.5, stroke: "black", strokeWidth: 1, fillRule: "evenodd"}} />;
+    this.state.meshDiagram = mesh.map(function (arr) { return [this.state.iCore[0] + arr[0], this.state.iCore[1] + arr[1]]; });
 };
 
 //Stats 2 and 3 are the same everytime
@@ -134,19 +95,19 @@ var setupStats = function(props)
     var v2Polygon;
     var v2FirstPoint;//Done to save an operation step
     /*Iterate For Each Triangle*/
-    for (var i = 0; i < chart.iParts; i++)
+    for (var i = 0; i < this.state.Quantity; i++)
     {
         //Adjust Angles
         if(i === 0)//First Stat
         {
-            v2Polygon = [new Vector2(0,0), new Vector2(0, (-statData.Values[0]*(1/statData.Ranges[i][1])*chart.iScale)), new Vector2(0, (-statData.Values[1]*(1/statData.Ranges[i][1])*chart.iScale))];
-            v2FirstPoint = Coll.v2Rotate2D(v2Polygon[1],v2Polygon[0],iDegrees*i);/*The Current Stat Point*/
-            Coll.v2Rotate2D(v2Polygon[2],v2Polygon[0],iDegrees*(i+1));/*The Next Stat Point*/
+            v2Polygon = [new Vector2(0,0), new Vector2(0, (-this.state.Values[0]*(1/this.state.Ranges[i][1])*iDiagramScale)), new Vector2(0, (-this.state.Values[1]*(1/this.state.Ranges[i][1])*iDiagramScale))];
+            v2FirstPoint = Coll.v2Rotate2D(v2Polygon[1],v2Polygon[0],this.state.iDegrees*i);/*The Current Stat Point*/
+            Coll.v2Rotate2D(v2Polygon[2],v2Polygon[0],this.state.iDegrees*(i+1));/*The Next Stat Point*/
         }
-        else if(i !== chart.iParts-1)//Mid Stats
+        else if(i !== this.state.Quantity-1)//Mid Stats
         {
-            v2Polygon = [new Vector2(0,0), v2Polygon[2], new Vector2(0, (-statData.Values[i+1]*(1/statData.Ranges[i+1][1])*chart.iScale))];
-            Coll.v2Rotate2D(v2Polygon[2],v2Polygon[0],iDegrees*(i+1));/*The Next Stat Point*/
+            v2Polygon = [new Vector2(0,0), v2Polygon[2], new Vector2(0, (-this.state.Values[i+1]*(1/this.state.Ranges[i+1][1])*iDiagramScale))];
+            Coll.v2Rotate2D(v2Polygon[2],v2Polygon[0],this.state.iDegrees*(i+1));/*The Next Stat Point*/
         }
         else//Last Stat
         {
@@ -155,21 +116,16 @@ var setupStats = function(props)
         mesh.push([v2Polygon[0].x, v2Polygon[0].y]); mesh.push([v2Polygon[1].x, v2Polygon[1].y]); mesh.push([v2Polygon[2].x, v2Polygon[2].y]);
     }    
     //Offset Coordinates
-    meshTrans = mesh.map(function (arr) { return [iCore[0] + arr[0], iCore[1] + arr[1]]; });
-
-    //Draw Stats
-    var htmlStats = <polygon points={meshTrans} fill = "url(#grad)" style={{fillOpacity: 0.66, stroke: "red", strokeWidth: 0, fillRule: "evenodd"}} />;
-
-    return htmlStats;
+    this.state.meshStats = mesh.map(function (arr) { return [this.state.iCore[0] + arr[0], this.state.iCore[1] + arr[1]]; });
 };
 
 var SetupTextAndTicks = function(props)
 {
     var htmlResult = [];
-    for (var i = 0; i < chart.iParts; i++)
+    for (var i = 0; i < this.state.Quantity; i++)
     {   
         /*Stat Type Label*/
-        var iAngle = i*iDegrees;
+        var iAngle = i*this.state.iDegrees;
 
         var iFlip = 0;
         //Rotate text around it's center if upsidedown
@@ -177,14 +133,14 @@ var SetupTextAndTicks = function(props)
         {
             iFlip = 180;
         }        
-        var typeCenter = [iCore[0], iCore[1]-chart.iScale];
+        var typeCenter = [this.state.iCore[0], this.state.iCore[1]-iDiagramScale];
         htmlResult.push( 
-        <text text-anchor="middle" style={{stroke: "rgb(0,0,0)", fontSize: 14, strokeWidth: 1}} x={typeCenter[0]} y={typeCenter[1]-6} transform={"rotate("+iAngle+", "+iCore[0]+","+iCore[1]+"), rotate("+iFlip+","+typeCenter[0]+","+(typeCenter[1]-10)+")"} > 
-        {statData.Types[i]}
+        <text text-anchor="middle" style={{stroke: "rgb(0,0,0)", fontSize: 14, strokeWidth: 1}} x={typeCenter[0]} y={typeCenter[1]-6} transform={"rotate("+iAngle+", "+this.state.iCore[0]+","+this.state.iCore[1]+"), rotate("+iFlip+","+typeCenter[0]+","+(typeCenter[1]-10)+")"} > 
+        {this.state.Types[i]}
         </text>);
 
         htmlResult.push( 
-        <text text-anchor="middle" style={{stroke: "rgb(0,0,0)", fontSize: 40, strokeWidth: 1}} x={typeCenter[0]} y={typeCenter[1]-40} transform={"rotate("+iAngle+", "+iCore[0]+","+iCore[1]+"), rotate("+-iAngle+","+typeCenter[0]+","+(typeCenter[1]-50)+")"} > 
+        <text text-anchor="middle" style={{stroke: "rgb(0,0,0)", fontSize: 40, strokeWidth: 1}} x={typeCenter[0]} y={typeCenter[1]-40} transform={"rotate("+iAngle+", "+this.state.iCore[0]+","+this.state.iCore[1]+"), rotate("+-iAngle+","+typeCenter[0]+","+(typeCenter[1]-50)+")"} > 
         {gradeCalc(i)}
         </text>);
 
@@ -192,48 +148,74 @@ var SetupTextAndTicks = function(props)
         var ticks = 10;
         for(var iT = 1; iT<ticks; iT++)
         {
-            htmlResult.push(<line x1={-4+iCore[0]} y1={iCore[1]-((chart.iScale/ticks)*iT)} x2={4+iCore[0]} y2={iCore[1]-((chart.iScale/ticks)*iT)} style={{stroke: "rgb(0,0,0)", strokeWidth: 1}} transform={"rotate("+iAngle+", "+iCore[0]+","+iCore[1]+")"} />);
+            htmlResult.push(<line x1={-4+this.state.iCore[0]} y1={this.state.iCore[1]-((iDiagramScale/ticks)*iT)} x2={4+this.state.iCore[0]} y2={this.state.iCore[1]-((iDiagramScale/ticks)*iT)} style={{stroke: "rgb(0,0,0)", strokeWidth: 1}} transform={"rotate("+iAngle+", "+this.state.iCore[0]+","+this.state.iCore[1]+")"} />);
            
             if((iT%2) === 0)
             {
                 //Deinfe and round off the tick values
-                var tickValue = Math.round(100 * (iT*( statData.Ranges[i][1]/ticks)) )/100;
-                typeCenter = [iCore[0]+5, iCore[1]-((chart.iScale/ticks)*iT)+3];
+                var tickValue = Math.round(100 * (iT*( this.state.Ranges[i][1]/ticks)) )/100;
+                typeCenter = [this.state.iCore[0]+5, this.state.iCore[1]-((iDiagramScale/ticks)*iT)+3];
                 htmlResult.push(
-                <text x={typeCenter[0]} y={typeCenter[1]} style={{stroke: "rgb(0,0,0)", strokeWidth: 1}} transform={"rotate("+iAngle+", "+(iCore[0])+","+iCore[1]+"), rotate("+iFlip+","+(typeCenter[0]-5)+","+(typeCenter[1]-3)+")"}>
+                <text x={typeCenter[0]} y={typeCenter[1]} style={{stroke: "rgb(0,0,0)", strokeWidth: 1}} transform={"rotate("+iAngle+", "+(this.state.iCore[0])+","+this.state.iCore[1]+"), rotate("+iFlip+","+(typeCenter[0]-5)+","+(typeCenter[1]-3)+")"}>
                     {tickValue}
                 </text>);
             }
         }
     }
-    return <a href="">{htmlResult}</a>
+    this.state.htmlText = <a href="">{htmlResult}</a>
 };
 
 class Diagram extends React.Component
 {
+    exampleFunction = () => {this.render();}
+    
     constructor(props){
         super(props);
+        this.state = 
+        {
+            //User inputted stats as semi-colin dilimited strings
+                Quantity: null,
+                MaxPoints:  100,
+                Ranges:     [[0,8], [0,10], [0,7],      [0,10],        [0,9],       [0,2],      [0,10], [0,10], [0,10], [0,10]],
+                Types:      ["POWER","SPEED","RANGE",   "DURABILITY",  "PRECISION", "POTENTIAL","???"],
+                Values:     [1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+                
+                letterGrades:   ['F','E','D','C','B','A','S','?'],
+                statGrades:     [0,0,0,0,0,0,0,0,0,0,0,0,0],
+            
+                //Window Dimensions Window Center
+                iDegrees: (360/this.state.Quantity),
+                iDim: [iDiagramScale*3, iDiagramScale*3],
+                iCore: [null, null],
+                meshDiagram: null,
+                meshStats: null,
+                htmlText: null,
+        },        
+        this.state.iCore = [this.state.iDim[0]/2, this.state.iDim[1]/2];
+        setupDiagram;
+        setupStats;
+        SetupTextAndTicks;
     };
 
     render(){
         return(
             <div>
                 <div>
-                    <svg width={iDim[0]} height={iDim[1]} >
-                        <circle cx={iCore[0]} cy={iCore[1]} r={1*chart.iScale} style={{fill: "white", fillOpacity: 0.5, stroke: "black", strokeWidth: 2}} />
+                    <svg width={this.state.iDim[0]} height={this.state.iDim[1]} >
+                        <circle cx={this.state.iCore[0]} cy={this.state.iCore[1]} r={1*iDiagramScale} style={{fill: "white", fillOpacity: 0.5, stroke: "black", strokeWidth: 2}} />
                         <defs>
                             <linearGradient id = "grad">
                                 <stop offset="0" stop-color="purple"/>
                                 <stop offset="1" stop-color="lightBlue"/>
                             </linearGradient>
                         </defs>
-                        {setupDiagram()}
-                        {setupStats()}
-                        {SetupTextAndTicks()}
+                        {<polygon points={this.state.meshDiagram} style={{fill: "white", fillOpacity: 0.5, stroke: "black", strokeWidth: 1, fillRule: "evenodd"}} />}
+                        {<polygon points={this.state.meshStats} fill = "url(#grad)" style={{fillOpacity: 0.66, stroke: "red", strokeWidth: 0, fillRule: "evenodd"}} />}
+                        {this.state.htmlText}
                     </svg>
                 </div>
-                <Box display="flexbox" border="2px solid #3f0000" bgcolor="darkGrey" color="#a4a4a4" width={iDim[0]} height={iDim[1]}>
-                    <StatInputCtrls></StatInputCtrls>
+                <Box display="flexbox" border="2px solid #3f0000" bgcolor="darkGrey" color="#a4a4a4" width={this.state.iDim[0]} height={this.state.iDim[1]}>
+                    <StatInputCtrls exampleFunctionLink={this.exampleFunction} value1={this.state.Quantity}></StatInputCtrls>
                 </Box>
             </div>
         );
--- src/Components/StatInputCtrls.js
+++ src/Components/StatInputCtrls.js
@@ -1,33 +1,54 @@
-import React from "react";
+import React, {useState} from "react";
 import { makeStyles } from '@material-ui/core/styles';
 import { Box } from '@material-ui/core';
 import Diagram from "./Diagram.js";
 
+var stats = 3;
+
+var statCtrls = function(props) 
+{
+    var controls = [];
+
+    for(var i=0; i < 3; i++)
+    {
+        controls.push(<div>
+            <label style = {{color: "black"}} >Stat: </label>
+            <input type="input" name="Stat" style = {{width: "96px"}}></input>
+            
+            <label style = {{color: "black"}} > Value: </label>
+            <input type="number" name="Value" style = {{width: "48px"}}></input>
+            
+            <label style = {{color: "black"}} > Range: </label>
+            <input type="number" name="Min" value={0} style = {{width: "48px"}}></input>
+            <input type="number" name="Max" value={100} style = {{width: "48px"}}></input>
+
+        </div>)
+    }
+    return controls;
+}
+
 //User Input Class
 class StatInputCtrls extends React.Component
-{
-    constructor(props)
-    {
-        super(props);
-        this.state = 
-        {
-            value: '3'
-        };
-        //OnHandle Updates
-        this.handleChange = this.handleChange.bind(this);
-    }
-
-    handleChange(event) 
-    {
-        this.setState({value: event.target.value});//chart.iParts = event.target.value
-    };  
-    
+{    
     render() 
     {
         return(        
         <div>
-            <label style = {{color: "black"}} >Stat quantity: </label>
-            <input type="number" style = {{width: "38px"}} value={this.state.value} onChange={this.handleChange}></input>
+            <div>
+                <label style = {{color: "black"}} >Stat quantity: </label>
+
+                <input readOnly={false} defaultValue={8} 
+                type="number" name="SQ"
+                onChange={this.props.exampleFunctionLink}
+                value={this.props.value1} 
+                style = {{width: "38px"}} ></input>
+
+            </div>
+            <div>
+                <label style = {{color: "black"}} >Max Points/Levels: </label>
+                <input type="number" style = {{width: "38px"}} value={100}></input>
+            </div>
+            {statCtrls()}
         </div>);
     }
 };
