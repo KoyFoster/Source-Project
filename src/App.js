133a708519447aa3778b67cc16d0a508df3ba867
KoyFoster <koyyashina@hotmail.com>
2020-01-09 10:46:30 -0600

    Working Themes Started

--- src/App.js
+++ src/App.js
@@ -2,21 +2,95 @@ import React from "react";
 import "./App.css";
 import StatCard from './Components/StatCard.js';
 
-//import Col from './Components/Grid/Col'
-//import Row from './Components/Grid/Row'
+import {Button, MuiThemeProvider, createMuiTheme} from '@material-ui/core'
 
-/*var gProfileBackground= "url('https://steamcdn-a.akamaihd.net/steamcommunity/public/images/items/329050/46014c822560d7f56ae1081310d694fdd51a3a57.jpg')"*/
+
+//Styles
+const theme = createMuiTheme({
+  overrides: {
+      MuiButton:
+      {
+        contained: {          
+          background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+          border: 0,
+          borderRadius: 3,
+
+          boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+          color: 'white',
+          padding: '0 30px',
+        }
+      },w
+      MuiTextField:
+      {
+        contained: {          
+          background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+          border: 0,
+          borderRadius: 3,
+
+          boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+          color: 'white',
+          padding: '0 30px',
+        }
+      },
+
+
+  },
+
+      //Global style that effects all sub styles
+      /*'@global': 
+      {
+          //If you nest themes, You should target [class*="MuiButton-root"].
+          '.MuiButton-root': {
+            fontSize: '1rem',
+          },
+          '.MuiInputLabel-root': {
+            fontSize: '1rem',
+  
+            padding: '6px 6px',
+            borderRadius: 3
+          },
+          '.MuiTextField-root': {
+              fontSize: '1px',
+              width: '152px'
+          },
+      },
+      root: {
+          fontSize: '16px',
+      },
+      checked: {},
+      
+      Label: {  
+          textAlign: 'center',        
+          backgroundColor: 'black',
+          color: 'white',
+      },
+  
+      Field: {
+          textAlign: 'center',
+      },
+      NumberField: {  
+          width: '56px',
+          textAlign: 'center'
+      },
+  
+      Button: {
+      },*/
+  });
+
+//Global Style
+const Global = withStyle({
+
+
+});
+
 
 /* ---------------------- CORE ---------------------- */
 class App extends React.Component {
   render() {
   return (
-    <div>
-    <StatCard></StatCard>
-
-
-
-    </div>
+    <MuiThemeProvider theme={theme}>
+      <StatCard></StatCard>
+    </MuiThemeProvider>
   )};
 }
 
--- src/Components/StatInputForm.js
+++ src/Components/StatInputForm.js
@@ -1,65 +1,12 @@
 import React from "react";
-import { Button, TextField, makeStyles, InputLabel, Divider, createMuiTheme, ThemeProvider } from '@material-ui/core';
+import { Button, TextField, makeStyles, InputLabel, Divider } from '@material-ui/core';
 import {Row, Col} from './Grid.js'
 import { purple, orange, green, red } from "@material-ui/core/colors";
-
-//Styles
-const useStyles = makeStyles(theme =>
-({
-    //Global style that effects all sub styles
-    '@global': 
-    {
-        //If you nest themes, You should target [class*="MuiButton-root"].
-        '.MuiButton-root': {
-          fontSize: '1rem',
-        },
-        '.MuiInputLabel-root': {            
-          fontSize: '1rem',
-
-          padding: '6px 6px',
-          borderRadius: 3
-        },
-        '.MuiTextField-root': {
-            fontSize: '1px',
-            width: '152px'
-        },
-    },
-    root: {
-        fontSize: '16px',
-    },
-    checked: {},
-    
-    Label: {  
-        textAlign: 'center',        
-        backgroundColor: 'black',
-        color: 'white',
-    },
-
-    Field: {
-        textAlign: 'center',
-    },
-    NumberField: {  
-        width: '56px',
-        textAlign: 'center'
-    },
-
-    Button: {
-      background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
-      border: 0,
-      borderRadius: 3,
-
-      boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
-      color: 'white',
-      padding: '0 30px',
-    },
-}));
+import "../App.css";
 
 //User Input Class
 function StatInputForm(props)
 {
-    //Set Styles Class
-    const classes = useStyles();
-
     //On Slices Change, update form
     var RandomizeStats = (event) =>
     {
@@ -91,9 +38,9 @@ function StatInputForm(props)
                 for(iRow=-1; iRow < rows; iRow++)
                 {
                     if(iRow < 0)//Run if else cases if iRow is not negative
-                    htmlBuffer.push(<InputLabel classes={{root: classes.Label}} > Stat </InputLabel>);
+                    htmlBuffer.push(<InputLabel > Stat </InputLabel>);
                     else
-                    htmlBuffer.push(<TextField inputProps={{style: { textAlign: "center" }}} name={"Types"+iRow} value={props.Types[iRow]} onChange={(event) => Update(event)} ></TextField>);
+                    htmlBuffer.push(<TextField name={"Types"+iRow} value={props.Types[iRow]} onChange={(event) => Update(event)} ></TextField>);
                 }
                 htmlForm.push(  <Col name={"Row"+iRow}> {htmlBuffer} </Col> );
                 htmlForm.push(  <Divider orientation="vertical" /> );
@@ -103,9 +50,9 @@ function StatInputForm(props)
                 for(iRow=-1; iRow < rows; iRow++)
                 {
                     if(iRow < 0)//Run if else cases if iRow is not negative
-                    htmlBuffer.push(<InputLabel classes={{root: classes.Label}} > Value </InputLabel>);
+                    htmlBuffer.push(<InputLabel > Value </InputLabel>);
                     else
-                    htmlBuffer.push(<TextField classes={{root: classes.NumberField}} type="number" name={"Value"+iRow} value={props.Values[iRow]} onChange={(event) => Update(event)} ></TextField>);
+                    htmlBuffer.push(<TextField type="number" name={"Value"+iRow} value={props.Values[iRow]} onChange={(event) => Update(event)} ></TextField>);
                 }
                 htmlForm.push(  <Col name={"Row"+iRow}> {htmlBuffer} </Col> );
                 htmlForm.push(  <Divider orientation="vertical" /> );
@@ -115,9 +62,9 @@ function StatInputForm(props)
                 for(iRow=-1; iRow < rows; iRow++)
                 {
                     if(iRow < 0)//Run if else cases if iRow is not negative
-                    htmlBuffer.push(<InputLabel classes={{root: classes.Label}} > Min </InputLabel>);
+                    htmlBuffer.push(<InputLabel  > Min </InputLabel>);
                     else
-                    htmlBuffer.push(<TextField classes={{root: classes.NumberField}} type="number" name={"RangesMin"+iRow} value={props.Ranges[iRow][0]} onChange={(event) => Update(event)} ></TextField>);
+                    htmlBuffer.push(<TextField  type="number" name={"RangesMin"+iRow} value={props.Ranges[iRow][0]} onChange={(event) => Update(event)} ></TextField>);
                 }
                 htmlForm.push(  <Col name={"Row"+iRow}> {htmlBuffer} </Col> );
             }
@@ -126,23 +73,23 @@ function StatInputForm(props)
                 for(iRow=-1; iRow < rows; iRow++)
                 {
                     if(iRow < 0)//Run if else cases if iRow is not negative
-                    htmlBuffer.push(<InputLabel classes={{root: classes.Label}} > Max </InputLabel>);
+                    htmlBuffer.push(<InputLabel  > Max </InputLabel>);
                     else
-                    htmlBuffer.push(<TextField classes={{root: classes.NumberField}} type="number" name={"RangesMax"+iRow} value={props.Ranges[iRow][1]} onChange={(event) => Update(event)} ></TextField>);
+                    htmlBuffer.push(<TextField  type="number" name={"RangesMax"+iRow} value={props.Ranges[iRow][1]} onChange={(event) => Update(event)} ></TextField>);
                 }
                 htmlForm.push(  <Col name={"Row"+iRow}> {htmlBuffer} </Col> );
             }
         }
-        return <Col><Button classes={{root: classes.Button}} name="RNG" onClick={(event) => RandomizeStats(event)} > RNG </Button><Row>{htmlForm}</Row></Col>;
+        return <Col><Button variant="contained" name="RNG" onClick={(event) => RandomizeStats(event)} > RNG </Button><Row>{htmlForm}</Row></Col>;
     }
 
         return(
-        <ThemeProvider>
+        <div>
             <div name="GraphBody" style={{display: 'flex', justifyContent:'center'}}>
             <Row>
                 <Col>
-                    <InputLabel classes={{root: classes.Label}} > Stat Quantity </InputLabel>
-                    <TextField classes={{root: classes.NumberField}} 
+                    <InputLabel  > Stat Quantity </InputLabel>
+                    <TextField 
                         type="number" name="Quantity"
                         value={props.Quantity}
                         onChange={(event) => Update(event)}
@@ -150,10 +97,10 @@ function StatInputForm(props)
                     </TextField>
                 </Col>
                 <Col>
-                    <InputLabel classes={{root: classes.Label}} > Points Spent: {props.PointTotal} </InputLabel>                    
+                    <InputLabel  > Points Spent: {props.PointTotal} </InputLabel>                    
                     <Row justifyContent="left">
-                        <InputLabel classes={{root: classes.Label}} > Limit: </InputLabel>
-                        <TextField classes={{root: classes.NumberField}}
+                        <InputLabel > Limit: </InputLabel>
+                        <TextField 
                             type="number" name="Limit"
                             value={props.PointLimit}
                             onChange={(event) => RandomizeStats(event)}
@@ -165,7 +112,7 @@ function StatInputForm(props)
             <div name="FormBody" align="center">
                 {statForm(props)}
             </div>
-        </ThemeProvider >);
+        </div >);
 };
 
 export default StatInputForm;
