ed54a3092f3257146e30e47db19e1b0e2276cca2
KoyFoster <koyyashina@hotmail.com>
2020-10-24 22:55:25 -0500

    Created functions for updated object keys

--- src/Components/Test2.js
+++ src/Components/Test2.js
@@ -1,9 +1,95 @@
 import React from 'react';
 // import Drag from './Forms/Drag';
-// import TreeDropDown from './Forms/TreeDropDown';
+import { defaultTemplates as Templates } from './Templates';
 
 const Test2 = () => {
-  return <div>asdasd</div>;
+  const replaceVar = (newKey, oldKey, varObj) => {
+    // console.warn(`replaceVar:`, { oldKey, newKey, varObj });
+    if (varObj.vars) {
+      const obj = JSON.parse(varObj.vars);
+      Object.keys(obj).forEach((key2) => {
+        // Cross compare against oldKey
+        // Size check
+        const pos = oldKey.length - 1;
+        let bUpdate = false;
+        if (oldKey.length > obj[key2].length) {
+        } else {
+          let i = 0;
+          let bMatch = true;
+
+          obj[key2].forEach((k) => {
+            if (obj[key2][i] !== oldKey[i]) {
+              bMatch = false;
+            }
+            if (bMatch && i === pos) {
+              // console.warn(`${obj[key2][i]} =/= ${newKey[i]}`);
+              obj[key2][i] = newKey[i];
+              bUpdate = true;
+            }
+
+            i += 1;
+          });
+        }
+        // update vars
+        if (bUpdate) {
+          varObj.vars = JSON.stringify(obj);
+          console.warn(`Match Found:`, varObj);
+        }
+      });
+    }
+  };
+
+  const UpdateAllKeys = (newKey, oldKey, data) => {
+    // newKey should be the same length as the old Key
+    if (newKey.length !== oldKey.length) return;
+
+    // Iterate through entire dataobject
+    data.Values.forEach((Value) => {
+      Object.keys(Value.Values).forEach((key) => {
+        // 'Points' is the only variable carrying field at this depth
+        if (Value.Values[key].Points) {
+          // console.warn('Points:', Value.Values[key].Points);
+          replaceVar(newKey, oldKey, Value.Values[key].Points);
+        }
+
+        // ITERATE through Num/Min/Max vars
+        Value.Values[key].Values.forEach((val) => {
+          if (val.Num.vars) {
+            replaceVar(newKey, oldKey, val.Num);
+          }
+          if (val.Min.vars) {
+            replaceVar(newKey, oldKey, val.Min);
+          }
+          if (val.Max.vars) {
+            replaceVar(newKey, oldKey, val.Max);
+          }
+        });
+      });
+    });
+
+    return data;
+  };
+
+  const data = { ...Templates['ArcheAge (V2020.10.13)'] };
+
+  return (
+    <div>
+      {JSON.stringify(
+        UpdateAllKeys(
+          [
+            'Values',
+            'Page One',
+            'Values',
+            'Primary Stats',
+            'Values',
+            'Big Butts',
+          ],
+          ['Values', 'Page One', 'Values', 'Primary Stats', 'Values', 'Spirit'],
+          data,
+        ),
+      )}
+    </div>
+  );
 };
 
 export default Test2;
